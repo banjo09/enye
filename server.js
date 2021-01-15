bfbc1ba8e8d787ce3fb95ee08a4322e3965aa1e2
banjo09 <olabanjoolaleye@gmail.com>
2021-01-15 16:13:31 +0100

    enye GITHUB FIRST PUSH

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-app.listen(9000);
-console.log('server is running on : 127.0.0.1: 9000')
+const port = process.env.PORT || 9000
+app.listen(port);
+console.log('server is running on : 127.0.0.1: ' + port)
