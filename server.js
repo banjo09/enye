7101777fd13cbe9380b54dadd892d0a7fcb8616d
banjo09 <olabanjoolaleye@gmail.com>
2021-01-15 19:37:03 +0100

    enye GITHUB second PUSH

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 9000
-app.listen(port);
-console.log('server is running on : 127.0.0.1: ' + port)
+const PORT = process.env.PORT || 9000
+app.listen(PORT);
+console.log('enye server is running on : 127.0.0.1: ' + PORT)
